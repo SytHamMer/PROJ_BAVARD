b731d46a48f91cdb903c182f181ac82828fedd83
lilcdx <113095135+lilcdx@users.noreply.github.com>
2023-05-02 10:42:52 +0200

    [ADD]: Interface Concierge + new argument messageReceived for concierge

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,18 @@ public class Main {
 
         Batiment polytech = new Batiment();
         polytech.setConcierge("Concierge");
-        polytech.addBavards("Lila","Lila_mdp",false);
+        polytech.addBavards("Lila","Lila_mdp",true);
         Bavard mathys = new Bavard("Mathys","dr(sd-ftèyg_huiojp",true);
         polytech.addBavards(mathys);
         Bavard lila = polytech.getBavard("Lila");
         polytech.addBavards("Sorana_Cimpan","uigyvuftèygui",true);
         lila.generateMessage("Test","Ceci est mon message",lila.getUsername());
+        mathys.generateMessage("Chiant", "quoicoube", mathys.getUsername());
 
-        ConnexionInterface mainFrame = new ConnexionInterface(polytech);
+        //ConnexionInterface mainFrame = new ConnexionInterface();
+
+        System.out.println(polytech.getConcierge().getMessageReceived());
+
+        ConciergeInterface mainFrame = new ConciergeInterface(polytech);
     }
 }
