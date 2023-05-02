c1b1e39705d99a6a8dd39de51a12277ef090d797
lilcdx <113095135+lilcdx@users.noreply.github.com>
2023-05-02 11:07:38 +0200

    [ADD]: Debut Interface Bavard (liste messages + texte raccourci)

--- src/BavardInterface.java
+++ src/BavardInterface.java
@@ -1,26 +1,28 @@
 import javax.swing.*;
-import java.awt.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class ConciergeInterface extends JFrame {
-    private Batiment bat;
-    private Concierge concierge;
-    public ConciergeInterface(Batiment bat){
-        this.bat = bat;
-        this.concierge = bat.getConcierge();
+public class BavardInterface extends JFrame {
+    private Bavard bav;
+
+    public BavardInterface(Bavard bav){
+        this.bav = bav;
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500, 320);
 
         JLabel title = new JLabel("Interface Concierge");
         this.add(title);
-        ArrayList<HashMap<String,String>> messages = concierge.getMessageReceived();
+        ArrayList<HashMap<String,String>> messages = bav.getMessageReceived();
         ArrayList<String> modeleList = new ArrayList<>();
         for(HashMap<String,String> m : messages){
+            System.out.println(m);
             String sujet = m.get("sujet");
             String auteur = m.get("auteur");
             String texte = m.get("text");
+            if (texte.length()>=15) {
+                texte = texte.substring(0, 14) + "...";
+            }
             modeleList.add("[" + sujet + "] " + auteur + " : " + texte);
         }
         JList messList = new JList<>(modeleList.toArray());
