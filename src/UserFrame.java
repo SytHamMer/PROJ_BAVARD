c68ce5e3bcb1a7ca208a7150f44bb93d9e6565b0
SytHamMer <mathys.lebon1@gmail.com>
2023-05-09 15:34:59 +0200

    [ADD]: Get selected CheckBox in UserFrame

--- src/UserFrame.java
+++ src/UserFrame.java
@@ -1,9 +1,14 @@
 
 import javax.swing.*;
         import java.awt.*;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class UserFrame extends JFrame {
+
+    private JPanel choicePanel;
+    private ArrayList<String> selectedUsers = new ArrayList<>();
+
 
     public UserFrame(Batiment bat){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -14,10 +19,9 @@ public class UserFrame extends JFrame {
         int nbCheck = values.size();
         int nbCol = nbCheck / 5;
         System.out.println(nbCol);
-        JPanel choicePanel = new JPanel(new GridLayout(5,nbCol));
-        System.out.println("mathys a fait de la merde :" + values);
+        this.choicePanel = new JPanel(new GridLayout(5,nbCol));
         for(Bavard value : values){
-            choicePanel.add(new JCheckBox(value.getUsername()));
+            this.choicePanel.add(new JCheckBox(value.getUsername()));
         }
         this.add("Center", choicePanel);
         JButton valider = new JButton("Valider");
@@ -27,8 +31,36 @@ public class UserFrame extends JFrame {
         this.setVisible(true);
     }
 
+    public ArrayList<String> getSelectedUsers() {
+        return selectedUsers;
+    }
+
+    //Fonction appelé lorsque que l'on valide les thèmes sélectionnés
     public void validation(){
+        ArrayList<JCheckBox> checkboxes = getAllCheckBoxes(this.choicePanel);
+
+        for(JCheckBox checkbox : checkboxes){
+            if(checkbox.isSelected()){
+                this.selectedUsers.add(checkbox.getText());
+            }
+        }
+        System.out.println(this.selectedUsers);
         this.setVisible(false);
+    }
+
+    //Fonction qui récupère dans une liste l'ensemble des JCheckbox du panel
+    public static ArrayList<JCheckBox> getAllCheckBoxes(Container container) {
+        Component[] components = container.getComponents();
+        ArrayList<JCheckBox> result = new ArrayList<>();
+
+        for (Component component : components) {
+            if (component instanceof JCheckBox) {
+                result.add((JCheckBox) component);
+            } else if (component instanceof Container) {
+                result.addAll(getAllCheckBoxes((Container) component));
+            }
+        }
+        return result;
     }
 }
 
